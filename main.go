8ba15229ff6b135cbeacd38f6d57af246fe1256a
chairat_yo <chaidev.cm@gmail.com>
2022-01-27 08:37:47 +0700

    Added response to the form post

--- main.go
+++ main.go
@@ -44,6 +44,20 @@ func formHandler(writer http.ResponseWriter, request *http.Request) {
 		templates["form"].Execute(writer, formData{
 			Rsvp: &Rsvp{}, Errors: []string{},
 		})
+	} else if request.Method == http.MethodPost {
+		request.ParseForm()
+		responseData := Rsvp{
+			Name:       request.Form["name"][0],
+			Email:      request.Form["email"][0],
+			Phone:      request.Form["phone"][0],
+			WillAttend: request.Form["willattend"][0] == "true",
+		}
+		responses = append(responses, &responseData)
+		if responseData.WillAttend {
+			templates["thanks"].Execute(writer, responseData.Name)
+		} else {
+			templates["sorry"].Execute(writer, responseData.Name)
+		}
 	}
 }
 
@@ -53,8 +67,8 @@ func main() {
 	http.HandleFunc("/", welcomeHandler)
 	http.HandleFunc("/list", listHandler)
 
-	http.HandleFunc(("/form", formHandler)
-	
+	http.HandleFunc("/form", formHandler)
+
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		fmt.Println(err)
